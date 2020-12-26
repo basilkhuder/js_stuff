5b8cf255cd20f3b912cb89de9da2f10d72d062eb
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-12-25 23:33:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 Generates a random hex code
 */
 
-let hex_picker_gen = () => { 
+let hexGen = () => { 
     let hex_picker = ""
     const hex_color_codes = [
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
